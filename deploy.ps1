a6cc82ac7a109e8c3a4f963cb46b89a1fb83a6ad
Dan Muller <dan.muller@dmuller.us>
2022-10-09 09:16:18 -0400

    Added a comment to deploy.ps1.

--- deploy.ps1
+++ deploy.ps1
@@ -1,1 +1,2 @@
-Copy-Item ".\danm_looper.eel" "$env:APPDATA\REAPER\Effects\Experimental"
+# A quick and dirty overwrite to a well-known, existing folder
+Copy-Item ".\danm_looper.eel" "$env:APPDATA\REAPER\Effects\Experimental\"
